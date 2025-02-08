99a21ceb782cf1dfac69d1978818d39f961b5ac9
Lankitha Gallage <lankitha.gallage@infoprojects.nl>
2025-02-08 15:43:56 +0530

    RSVP-23 Set Up Dependency Injection (DI)

---
+++ api/Rsvp.Application/DependencyInjection.cs
@@ -0,0 +1,8 @@
+﻿namespace Rsvp.Application;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+  public static void AddApplicationServices(this IServiceCollection services) { }
+}
---
+++ api/Rsvp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿namespace Rsvp.Infrastructure;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+  public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration) { }
+}
