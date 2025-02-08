c679f9402f545748261ee8164367abdf2395bbe5
Lankitha Gallage <lankitha.gallage@infoprojects.nl>
2025-02-09 01:08:29 +0530

    RSVP-26 Implement Basic Exception Handling & Logging

--- api/Rsvp.Api/DependencyInjection.cs
+++ api/Rsvp.Api/DependencyInjection.cs
@@ -3,11 +3,23 @@
 using Rsvp.Application;
 using Rsvp.Infrastructure;
 
+using Serilog;
+
 public static class DependencyInjection
 {
   public static void AddServices(this IServiceCollection services, IConfiguration configuration)
   {
     services.AddApplicationServices();
     services.AddInfrastructureServices(configuration);
+  }
+
+  public static void AddLoggingServices(this WebApplicationBuilder builder)
+  {
+    // Add Serilog
+    Log.Logger = new LoggerConfiguration()
+      .ReadFrom.Configuration(builder.Configuration)
+      .CreateLogger();
+
+    builder.Host.UseSerilog();
   }
 }
