f5c8099e9f2125eb156e61f5444b515fac06337b
Lankitha Gallage <lankitha.gallage@infoprojects.nl>
2025-02-09 23:56:06 +0530

    RSVP-29 Install and Configure ORM

--- api/Rsvp.Infrastructure/DependencyInjection.cs
+++ api/Rsvp.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,16 @@
 ﻿namespace Rsvp.Infrastructure;
 
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using Rsvp.Infrastructure.Persistence;
+
 public static class DependencyInjection
 {
-  public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration) { }
+  public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+  {
+    services.AddDbContext<RsvpContext>(options =>
+      options.UseSqlServer(configuration.GetConnectionString("RsvpSqlDbConnection")));
+  }
 }
