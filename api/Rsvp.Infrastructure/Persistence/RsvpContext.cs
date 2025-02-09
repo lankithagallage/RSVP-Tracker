6e3f3e631370dafce1eed9a5e15d850c86541f62
Lankitha Gallage <lankitha.gallage@infoprojects.nl>
2025-02-09 23:56:06 +0530

    RSVP-29 Install and Configure ORM

---
+++ api/Rsvp.Infrastructure/Persistence/RsvpContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Rsvp.Infrastructure.Persistence;
+
+using Microsoft.EntityFrameworkCore;
+
+public class RsvpContext : DbContext
+{
+  public RsvpContext(DbContextOptions<RsvpContext> options) : base(options) { }
+}
