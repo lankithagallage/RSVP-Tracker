84aadbb32c97badf847126a1a1234cbfc4e5c01d
Lankitha Gallage <lankitha.gallage@infoprojects.nl>
2025-02-11 00:32:47 +0530

    RSVP-39 Configure Routing System

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -1,10 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideRouter([]),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            snapshot: { paramMap: { get: () => null } },
+          },
+        },
+      ],
     }).compileComponents();
   });
 
