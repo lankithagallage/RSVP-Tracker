2f37168d0fdca0762a2d8739ec0ff4a55ad20b25
Lankitha Gallage <lankitha.gallage@infoprojects.nl>
2025-02-13 01:39:51 +0530

    RSVP-55 BE: Add Ardalis.Result.AspNetCore.MvcOptionsExtensions to AddControllers in Program.cs

--- api/Rsvp.Api/Program.cs
+++ api/Rsvp.Api/Program.cs
@@ -1,24 +1,46 @@
+using System.Net;
+
+using Ardalis.Result;
+using Ardalis.Result.AspNetCore;
+
 using Rsvp.Api;
 using Rsvp.Api.Configuration;
 
-var builder = WebApplication.CreateBuilder(args);
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
 
-// Configurations
-builder.Configuration
-  .AddJsonFile("appsettings.Local.json", true, true)
-  .AddEnvironmentVariables();
+    // Configurations
+    builder.Configuration
+      .AddJsonFile("appsettings.Local.json", true, true)
+      .AddEnvironmentVariables();
 
-builder.AddLoggingServices();
+    builder.AddLoggingServices();
 
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
+    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+    builder.Services.AddProblemDetails();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddControllers(mvcOptions =>
+      {
+        mvcOptions.AddResultConvention(resultStatusMap =>
+          resultStatusMap.AddDefaultMap()
+            .For(ResultStatus.Ok, HttpStatusCode.OK, resultStatusOptions => resultStatusOptions
+              .For("POST", HttpStatusCode.Created)
+              .For("DELETE", HttpStatusCode.NoContent))
+            .For(ResultStatus.Invalid, HttpStatusCode.BadRequest)
+            .For(ResultStatus.Error, HttpStatusCode.InternalServerError)
+            .For(ResultStatus.NotFound, HttpStatusCode.NotFound)
+        );
+      });
 
-builder.Services.AddServices(builder.Configuration, builder.Environment.IsDevelopment());
+    builder.Services.AddServices(builder.Configuration, builder.Environment.IsDevelopment());
 
-var app = builder.Build().Configure();
+    var app = builder.Build().Configure();
 
-app.Run();
+    app.Run();
+  }
+}
