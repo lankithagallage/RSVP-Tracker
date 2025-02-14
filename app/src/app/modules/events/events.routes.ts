f66a1072cc3cfe8255ab92bbca4e7ea5a8ce6adc
Lankitha Gallage <lankitha.gallage@infoprojects.nl>
2025-02-14 12:34:01 +0530

    RSVP-64 FE: Implement Event Page

--- app/src/app/modules/events/events.routes.ts
+++ app/src/app/modules/events/events.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
-import { EventsComponent } from './events.component';
+import { EventsComponent } from './event-list/event-list.component';
+import { EventComponent } from './event/event.component';
 
-export const routes: Routes = [{ path: '', component: EventsComponent }];
+export const routes: Routes = [
+  { path: '', component: EventsComponent }, // Shows all events
+  { path: ':id', component: EventComponent }, // Shows a specific event by ID
+];
